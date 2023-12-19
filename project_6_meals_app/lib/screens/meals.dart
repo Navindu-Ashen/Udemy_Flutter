0450bc64d6ceec15e9e77ae0e885d9ba93e7bad0
Navindu Dissanayake <navindu158@gmail.com>
2023-12-20 00:37:41 +0530

    change

--- project_6_meals_app/lib/screens/meals.dart
+++ project_6_meals_app/lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_6_meals_app/main.dart';
 import 'package:project_6_meals_app/models/meal.dart';
+import 'package:project_6_meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -36,9 +37,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(
-          meals[index].title,
-        ),
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
       );
     }
     return Scaffold(
