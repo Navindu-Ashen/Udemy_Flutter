bd5c34fe0943c8b179bdd7a6478a53b52524728b
Navindu Dissanayake <navindu158@gmail.com>
2023-12-28 16:20:21 +0530

    final change

--- project_7_shopping_app/lib/widgets/grocery_list.dart
+++ project_7_shopping_app/lib/widgets/grocery_list.dart
@@ -29,37 +29,51 @@ class _GroceryListState extends State<GroceryList> {
       "flutter-test1-52276-default-rtdb.firebaseio.com",
       "shopping-app.json",
     );
-    final response = await http.get(url);
-    print(response.statusCode);
-    if (response.statusCode >= 400) {
+    try {
+      final response = await http.get(url);
+      // print(response.statusCode);
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = "Failed to fetch data. Please try again later.";
+        });
+      }
+
+      // print(response.body);
+
+      if (response.body == "null") {
+        setState(() {
+          isLoading = false;
+        });
+        return;
+      }
+
+      final Map<String, dynamic> listData = json.decode(response.body);
+
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+              (catItem) => catItem.value.title == item.value["category"],
+            )
+            .value;
+        loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value["name"],
+            quantity: item.value["quantity"],
+            category: category,
+          ),
+        );
+      }
       setState(() {
-        _error = "Failed to fetch data. Please try again later.";
+        _groceryItems = loadedItems;
+        isLoading = false;
       });
-      
+    } catch (error) {
+      setState(() {
+        _error = "Somthing went wrong.\nPlease Check your internet connection and try again.";
+      });
     }
-
-    final Map<String, dynamic> listData = json.decode(response.body);
-
-    final List<GroceryItem> loadedItems = [];
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-            (catItem) => catItem.value.title == item.value["category"],
-          )
-          .value;
-      loadedItems.add(
-        GroceryItem(
-          id: item.key,
-          name: item.value["name"],
-          quantity: item.value["quantity"],
-          category: category,
-        ),
-      );
-    }
-    setState(() {
-      _groceryItems = loadedItems;
-      isLoading = false;
-    });
   }
 
   void _addItem() async {
@@ -78,25 +92,67 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
     final itemIndex = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
-    ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: const Text("Item Removed!"),
-        action: SnackBarAction(
-          label: "Undo",
-          onPressed: () {
-            setState(() {
-              _groceryItems.insert(itemIndex, item);
-            });
-          },
-        ),
-      ),
+
+    final url = Uri.https(
+      "flutter-test1-52276-default-rtdb.firebaseio.com",
+      "shopping-app/${item.id}.json",
     );
+
+    final responce = await http.delete(url);
+
+    if (responce.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(itemIndex, item);
+      });
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).clearSnackBars();
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            "Failed to remove item. Try again later.",
+            style: TextStyle(
+                color: Colors.red, fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+        ),
+      );
+    } else {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).clearSnackBars();
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          duration: Duration(seconds: 3),
+          content: Text(
+            "Item Removed.",
+            style: TextStyle(
+              color: Color.fromARGB(255, 0, 0, 0),
+              fontSize: 14,
+            ),
+          ),
+        ),
+      );
+    }
+
+    // ScaffoldMessenger.of(context).clearSnackBars();
+    // ScaffoldMessenger.of(context).showSnackBar(
+    //   SnackBar(
+    //     content: const Text("Item Removed!"),
+    //     action: SnackBarAction(
+    //       label: "Undo",
+    //       onPressed: () {
+    //         setState(() {
+    //           _groceryItems.insert(itemIndex, item);
+    //         });
+    //       },
+    //     ),
+    //   ),
+    // );
   }
 
   @override
@@ -117,16 +173,16 @@ class _GroceryListState extends State<GroceryList> {
         itemBuilder: (ctx, index) => Dismissible(
           key: ValueKey(_groceryItems[index].id),
           onDismissed: (direction) {
-            removeItem(_groceryItems[index]);
+            _removeItem(_groceryItems[index]);
           },
           background: Container(
-            color: const Color.fromARGB(155, 250, 126, 118),
+            color: Color.fromARGB(155, 255, 91, 79),
             child: const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
                   Icons.delete,
-                  color: Color.fromARGB(255, 255, 17, 0),
+                  color: Color.fromARGB(255, 255, 255, 255),
                   size: 40,
                 ),
               ],
@@ -149,7 +205,11 @@ class _GroceryListState extends State<GroceryList> {
 
     if (_error != null) {
       content = Center(
-        child: Text(_error!, style: const TextStyle(fontSize: 16),),
+        child: Text(
+          _error!,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 16),
+        ),
       );
     }
 
